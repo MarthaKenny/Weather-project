0b8cd5203356bb450e74d383d34587ebfb170a7b
Martha Suleiman <suleimanmartha15@gmail.com>
2022-11-25 10:23:58 +0100

    implemented weather Api

--- index.js
+++ index.js
@@ -1,39 +1,67 @@
-function searchButton(event) {
-  event.preventDefault()
-    let citySearch = document.querySelector("#search-button");
-    let heading = document.querySelector("h1")
-    heading.innerHTML = citySearch.value
-}
-let searchIcon=document.querySelector("#search-form")
-searchIcon.addEventListener("submit", searchButton);
-
-
-
-let date = new Date
+let date = new Date();
 let hour = date.getHours();
-let minutes= date.getMinutes()
-let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "thursday", "Friday", "Saturday"];
+let minutes = date.getMinutes();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "thursday",
+  "Friday",
+  "Saturday",
+];
 let day = days[date.getDay()];
 let time = document.querySelector("h2");
-time.innerHTML = `${day}, ${hour}:${minutes}`
+time.innerHTML = `${day},${hour}:${minutes}`;
 
-function changefahrenheit(event) {
-  event.preventDefault();
-  let changedTemp = document.querySelector("#actualtemp");
-  let actualtemp = changedTemp.innerHTML;
-  changedTemp.innerHTML = (actualtemp * 9) / 5 + 32;
+function showTemperature(response) {
+  let temperature = Math.round(response.data.main.temp);
+
+  let weatherDescription = document.querySelector("#description");
+  weatherDescription.innerHTML = response.data.weather[0].description;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#humidity").innerHTML = Math.round(
+    response.data.main.humidity
+  );
+  document.querySelector("#max-temp").innerHTML = Math.round(
+    response.data.main.temp_max
+  );
+  let heading = document.querySelector("h1");
+  heading.innerHTML = response.data.name;
+
+  let temperatureElement = document.querySelector("#actualtemp");
+  temperatureElement.innerHTML = temperature;
 }
-let fahrenheitTemp = document.querySelector("#fahrenheit");
-fahrenheitTemp.addEventListener("click", changefahrenheit);
 
-
-
-
-function changeCelsius(event) {
-  event.preventDefault();
-  let changedTemp = document.querySelector("#actualtemp");
-  let actualtemp = changedTemp.innerHTML;
-  changedTemp.innerHTML = (changedTemp.innerHTML -32)* 5/9
+function search(city) {
+  let apiKey = "b95f179627c8dd37f41e1be6e3250e19";
+  let apiUrl = ` https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
 }
-let celsiusTemp = document.querySelector("#degree");
-celsiusTemp.addEventListener("click", changeCelsius);
+function searchLocation(position) {
+  let long = position.coords.longitude;
+  let lati = position.coords.latitude;
+  let apiKey = "b95f179627c8dd37f41e1be6e3250e19";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lati}&lon=${long}&units=metric&appid=${apiKey}`;
+  axios.get(apiUrl).then(showTemperature);
+}
+function getCurrentPosition(event) {
+  event.preventDefault();
+
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+function handleSearchButton(event) {
+  event.preventDefault();
+  let citySearch = document.querySelector("#search-button");
+  let city = citySearch.value;
+  search(city);
+}
+
+let searchIcon = document.querySelector("#search-form");
+searchIcon.addEventListener("submit", handleSearchButton);
+let currentLocationButton = document.querySelector("#current-location");
+currentLocationButton.addEventListener("click", getCurrentPosition);
+search("abuja");
